5661c874d3e9a98567f39e04f17c7630831975c7
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-03-21 00:48:14 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ExpansionTile(
           title: Text("This is an ExpansionTile"),
           subtitle: Text("ExpansionTile subtitle"),
-          trailing: Icon(_customIcon
-              ? Icons.arrow_drop_down_circle_outlined
-              : Icons.arrow_drop_down),
+          trailing: Icon(_customIcon? Icons.arrow_drop_down_circle_outlined : Icons.arrow_drop_down),
           children: [
             ListTile(
               title: Text('This is tile Number 2 '),
